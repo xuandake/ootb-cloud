bc371bd733a327190b152950a035c1c5bd418a00
xuandake <xuandake@163.com>
2020-07-06 23:23:30 +0800

    1、配置版本管理

--- ootb-gateway/src/main/java/com/ootb/gateway/GatewayApplication.java
+++
@@ -1,4 +0,0 @@
-package com.ootb.gateway;
-
-public class GatewayApplication {
-}
---
+++ ootb-gateway/src/main/java/com/ootb/gateway/OotbGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.ootb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OotbGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OotbGatewayApplication.class, args);
+    }
+}
